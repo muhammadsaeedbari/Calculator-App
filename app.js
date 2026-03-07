dbbb8ac84338fbf91ed3cb9f47412ea074e5574e
Muhammad Saeed Bari <muhammadsaeedbari@gmail.com>
2026-03-07 23:13:16 +0500

    first commit

--- app.js
+++ app.js
@@ -2,23 +2,30 @@
 
 
 function userClick(value){
-     userinp.value += value
-     userinp.innerText = userinp.value
-     console.log(userinp.value)
+
+    userinp.value += value
     
+    console.log((userinp.value));
+
  }
 
 function calculate(){
+   
+   try{
+     
     if(userinp.value==""){
         console.log("Enter Value")
         output.value = "Math Error"
     }
     else{
-        output.value= eval(userinp.value)
-        console.log(output.value)
-
-        
+        output.value= eval(userinp.value).toLocaleString()
+        console.log(output.value)    
     }
+   }
+   catch(value){
+   output.value = ("invalid")
+
+   }
 }
 
 function clearAll(){
@@ -31,16 +38,25 @@ function clearAll(){
 
 function rootnum() {
     
-    var currentVal = userinp.value;
+    // var currentVal = userinp.value;
 
-    if (currentVal === "") {
+    if (userinp.value === "") {
         output.value = "Enter Value";
     } else {
        
-        let result = Math.sqrt(eval(currentVal));
-        output.value = result;
-       
+        //let result = Math.sqrt(eval(currentVal));
+        //output.value = result;
+
+        output.value = Math.sqrt(eval(userinp.value))
+
     }
+}
+
+function clearOne(){
+    console.log("Work");
+    userinp.value = (userinp.value.slice(0, -1));
+    output.value = userinp.value;
+    //userinp.innetText = userinp.value;
 }
 
 
